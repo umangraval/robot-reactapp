168cf5adeadf8604682352b42a9b9c38f164fb96
umangraval <umangraval2000@gmail.com>
2019-06-19 18:22:44 +0530

    final

---
+++ build/precache-manifest.e482590e0330dc7b776a8ff32a76ea33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758f645c55121f2bce1e8692fd56dc86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "939a06f00950a5bdd27d",
+    "url": "/static/css/main.605d6a54.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "939a06f00950a5bdd27d",
+    "url": "/static/js/main.5498aa64.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
